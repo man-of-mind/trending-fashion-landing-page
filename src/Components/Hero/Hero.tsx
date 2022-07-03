202f743c50ca65082abd9539d4537a731ba531ac
Abiodun Ganiyu <timothyabiodun300@gmail.com>
2022-07-03 01:35:39 +0100

    Completed the hero section for the mobile screen

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -6,6 +6,7 @@ const Hero = () => {
         <section className={styles['hero']}>
             <article>
                 <h1>About Us</h1>
+                <hr></hr>
                 <p style={{paddingBottom: '20px', paddingTop: '10px'}}>She is a product manager who has seen numerous projects from 
                     inception to completion. I am presently at WhoGoHost as the product manager. </p>
                 <p>Here are some of the projects ive overseen so far in my career. 
