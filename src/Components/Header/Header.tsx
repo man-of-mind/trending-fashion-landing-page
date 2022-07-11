564283beb80d2ff89b5bc4167569141bc04a4816
Abiodun Ganiyu <timothyabiodun300@gmail.com>
2022-07-11 11:34:17 +0100

    Styles fixes and created navbar menu for mobile screen

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -5,6 +5,8 @@ import { ReactComponent as Menu } from "../../Assets/Icons/Hamburger.svg";
 import { ReactComponent as Arrow } from "../../Assets/Icons/Arrow.svg";
 
 const Header = () => {
+    const [showMenu, setShowMenu] = React.useState<boolean>(false);
+    
     return (
         <header className={styles['header-section']}>
             <nav>
@@ -19,9 +21,20 @@ const Header = () => {
                     </li>
                 </ul>
                 <i>
-                    <Menu />
+                    <Menu onClick={() => setShowMenu(!showMenu)} style={{cursor: 'pointer'}}/>
                 </i>
             </nav>
+            <div className={styles['mb-menu']} style={{ display: showMenu ? 'flex' : 'none', }}>
+                <ul>
+                    <li>Home</li>
+                    <li>About me</li>
+                    <li>Projects</li>
+                    <li>
+                        <span>Contact me</span>
+                        <Arrow style={{marginLeft: "5px", paddingBottom: "5px"}}/>
+                    </li>
+                </ul>
+            </div>
             <section>
                 <h1>Trendy Fashion</h1>
                 <ArrowDownIcon />
